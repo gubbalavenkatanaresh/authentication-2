5f8f6476fdf93d8bc8038d6624ec65da396ab42a
venkata naresh <venkatanaresh@gmail.com>
2023-03-20 09:13:36 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,26 @@
+// Write your JS code here
+
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Cart = props => {
+  const {history} = props
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    history.replace('/login')
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="cart-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+        />
+      </div>
+    </>
+  )
+}
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,25 @@
+// Write your JS code here
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Products = props => {
+  const {history} = props
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    history.replace('/login')
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="cart-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+export default Products
